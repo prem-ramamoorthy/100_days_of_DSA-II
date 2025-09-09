1091d1fe44a940bf3cb01b98753e4180a7f78ebf
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-09 09:45:20 +0530

    Day 76 : conpleted

---
+++ day76/kthLargestElement.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin() , nums.end() , greater<int>()) ;
+        return nums[k-1] ;
+    }
+
+int main() {
+    vector<int> nums = {3,2,1,5,6,4};
+    int k = 2;
+    cout << findKthLargest(nums, k) << endl; // Output: 5
+    return 0;
+}
